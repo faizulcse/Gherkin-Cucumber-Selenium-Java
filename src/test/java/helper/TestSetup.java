655ee44978c88203c049580ec390f5f2944b648c
fislam <fislam@rsglab.com>
2021-04-08 19:47:31 +0600

    update reports folder

--- src/test/java/helper/TestSetup.java
+++ src/test/java/helper/TestSetup.java
@@ -1,9 +1,6 @@
 package helper;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
